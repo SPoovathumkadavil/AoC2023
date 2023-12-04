1876acf666f6eca5fdb72faf40f489081ecec3b4
Saaleh <97705117+SPoovathumkadavil@users.noreply.github.com>
2023-12-04 13:36:34 -0600

    d4pt1

--- aoc/cuatro/DiaCuatro.java
+++ aoc/cuatro/DiaCuatro.java
@@ -22,7 +22,65 @@ public class DiaCuatro {
             e.printStackTrace();
         }
 
-        System.out.println(fileStrings);
+        int sum = pt1GetPointWorth(fileStrings);
+        System.out.println(sum);
+    }
+
+    public static int pt1GetPointWorth(ArrayList<String> strings) {
+
+        ArrayList<ScratchCard> games = new ArrayList<>();
+        int sum = 0;
+
+        for (String str : strings)
+            games.add(new ScratchCard(str));
+
+        for (ScratchCard game : games) {
+
+            for (int wn : game.winningNumbers) {
+                if (game.availableNumbers.contains(wn)) {
+                    game.pointWorth = (int) Math.pow(2, game.matching);
+                    game.matching++;
+
+                    /* 0, 1, 2, 3, 4 */
+                    /* 1, 2, 4, 8, 16 */
+
+                    game.points += game.pointWorth;
+
+                }
+            }
+            sum += game.pointWorth;
+            System.out.println(game.availableNumbers);
+        }
+
+        return sum;
+
+    }
+
+    static class ScratchCard {
+
+        int id;
+        int points = 0;
+        int pointWorth = 0;
+        int matching = 0;
+        ArrayList<Integer> winningNumbers = new ArrayList<>();
+        ArrayList<Integer> availableNumbers = new ArrayList<>();
+
+        public ScratchCard(String cardString) {
+
+            id = Integer.parseInt(cardString.split(":")[0].substring(5).trim());
+
+            String wns = cardString.substring(10, 39);
+            String ans = cardString.substring(42);
+
+            for (int i = 0; i < wns.split(" ").length; i++)
+                if (wns.split(" ")[i] != "")
+                    winningNumbers.add(Integer.parseInt(wns.split(" ")[i]));
+
+            for (int i = 0; i < ans.split(" ").length; i++)
+                if (ans.split(" ")[i] != "")
+                    availableNumbers.add(Integer.parseInt(ans.split(" ")[i]));
+
+        }
 
     }
 
